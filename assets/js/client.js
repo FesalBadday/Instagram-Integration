21ffa702fc6465e56e87e3f8370f91b098246266
FesalBadday <fesal.n04@hotmail.com>
2021-12-02 10:57:50 -0700

    updated function

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,7 +1,7 @@
-const init = async () => {
+const showRoom = async () => {
   const response = await fetch('/.netlify/functions/post');
   const data = await response.json();
-  const posts = data.filter((post) => post.caption.startsWith('#car'));
+  const posts = data.filter((post) => post.caption.split(' ')[0] === '#showroom');
 
   posts.forEach(post => {
     document.querySelector('.container').innerHTML += `
@@ -12,4 +12,4 @@ const init = async () => {
   });
 }
 
-init();
+showRoom();
